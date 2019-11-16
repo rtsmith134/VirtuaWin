a47fe9a8c38af9f19ee649e3b26ac29c19106e73
rtsmith134 <rtsmith134@gmail.com>
2019-11-16 16:19:01 -0700

    assorted changes

--- stdafx.h
+++ stdafx.h
@@ -10,8 +10,12 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
-
+#include <inspectable.h>
+// C RunTime Header Files
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
 #include <tchar.h>
 
 #include <string>
@@ -23,3 +27,4 @@
 
 
 // TODO: reference additional headers your program requires here
+//#define _WIN32_IE 0x0200
